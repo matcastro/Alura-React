d1870634fd1317314f6dc6b7bb238e336f4512f4
Mateus Castro <matcastro2004@yahoo.com.br>
2022-02-10 17:17:00 -0300

    Integrando css e organizando o projeto.

--- React/src/App.js
+++ React/src/App.js
@@ -1,13 +1,17 @@
+import React, { Component } from 'react';
 import FormularioCadastro from './components/FormularioCadastro';
 import ListaDeNotas from './components/ListaDeNotas'
+import "./assets/App.css"
 
-function App() {
-  return (
-    <section>
-      <FormularioCadastro></FormularioCadastro>
-      <ListaDeNotas></ListaDeNotas>
-    </section>
-  );
+class App extends Component {
+  render() {
+    return (
+      <section className='conteudo'>
+        <FormularioCadastro></FormularioCadastro>
+        <ListaDeNotas></ListaDeNotas>
+      </section>
+    );
+  }
 }
 
 export default App;
