d62938590f0fafda55156fa0410e1789617017d0
Mateus Castro <matcastro2004@yahoo.com.br>
2022-02-17 11:16:48 -0300

    implementando o padrão observable

--- React/src/App.js
+++ React/src/App.js
@@ -3,51 +3,28 @@ import FormularioCadastro from './components/FormularioCadastro';
 import ListaDeNotas from './components/ListaDeNotas'
 import ListaDeCategorias from './components/ListaDeCategorias'
 import "./assets/App.css"
+import Categorias from './dados/Categorias';
+import ArrayDeNotas from './dados/Notas';
 
 class App extends Component {
 
   constructor() {
     super()
-    this.notas = []
-    this.state = {
-      notas: [],
-      categorias: []
-    }
-  }
-
-  criarNota(titulo, texto, categoria) {
-    const novaNota = { titulo, texto, categoria }
-    const novoArrayNotas = [...this.state.notas, novaNota]
-    const novoEstado = {
-      notas: novoArrayNotas
-    }
-    this.setState(novoEstado)
-  }
-
-  adicionarCategoria(nomeCategoria) {
-    const novoArrayCategorias = [...this.state.categorias, nomeCategoria]
-    const novoEstado = {...this.state, categorias: novoArrayCategorias}
-    this.setState(novoEstado)
-  }
-
-  deletarNota(index) {
-    let arrayNotas = this.state.notas
-    arrayNotas.splice(index, 1)
-
-    this.setState({
-      notas: arrayNotas
-    })
+    this.categorias = new Categorias()
+    this.notas = new ArrayDeNotas()
   }
 
   render() {
     return (
       <section className='conteudo'>
-        <FormularioCadastro categorias={this.state.categorias} criarNota={this.criarNota.bind(this)}></FormularioCadastro>
+        <FormularioCadastro 
+        categorias={this.categorias.categorias} 
+        criarNota={this.notas.adicionarNota}></FormularioCadastro>
         <main className='conteudo-principal'>
           <ListaDeCategorias
-            adicionarCategoria={this.adicionarCategoria.bind(this)}
-            categorias={this.state.categorias}/>
-          <ListaDeNotas apagarNota={this.deletarNota.bind(this)} notas={this.state.notas}></ListaDeNotas>
+            adicionarCategoria={this.categorias.adicionarCategoria}
+            categorias={this.categorias.categorias}/>
+          <ListaDeNotas apagarNota={this.notas.apagarNota} notas={this.notas.notas}></ListaDeNotas>
         </main>
       </section>
     );
---
+++ React/src/dados/Notas.js
@@ -0,0 +1,32 @@
+export default class ArrayDeNotas {
+    constructor(){
+        this.notas = []
+        this._inscritos = []
+    }
+
+    inscrever(func){
+        this._inscritos.push(func)
+    }
+
+    notificar(){
+        this._inscritos.forEach(func => func(this.notas))
+    }
+
+    adicionarNota(titulo, texto, categoria) {
+        const novaNota = new Nota(titulo, texto, categoria)
+        console.log(novaNota)
+        this.notas.push(novaNota)
+    }
+
+    apagarNota(indice){
+        this.notas.splice(indice, 1)
+    }
+}
+
+class Nota {
+    constructor(titulo, texto, categoria){
+        this.titulo = titulo
+        this.texto = texto
+        this.categoria = categoria
+    }
+}
