50fe4bbd7afe52d33fc90dda4cf78b617d5ef0a0
Mateus Castro <matcastro2004@yahoo.com.br>
2022-02-17 15:17:36 -0300

    Implementando um projeto com function components e usando materialui para ter componentes com estilização pre pronta.

---
+++ React-Function/src/App.js
@@ -0,0 +1,16 @@
+import { Component, Fragment } from 'react';
+import './App.css';
+import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Formulário de Cadastro</h1>
+        <FormularioCadastro/>
+      </Fragment>
+    )
+  }
+}
+
+export default App;
