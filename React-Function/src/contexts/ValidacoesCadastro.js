24d672f4dfc24ef37194e2732b48431743761011
Mateus Castro <matcastro2004@yahoo.com.br>
2022-02-18 16:43:06 -0300

    validação default

--- React-Function/src/contexts/ValidacoesCadastro.js
+++ React-Function/src/contexts/ValidacoesCadastro.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const ValidacoesCadastro = React.createContext()
+const ValidacoesCadastro = React.createContext({
+     cpf: semValidacao, 
+     senha: semValidacao 
+})
+
+function semValidacao(){
+    return {
+        valido: true,
+        texto: ""
+    }
+}
 
 export default ValidacoesCadastro
