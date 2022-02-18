4a09dd173e237179ad1c19b16147e2e35dc15586
Mateus Castro <matcastro2004@yahoo.com.br>
2022-02-18 14:46:49 -0300

    Reutilização de componentes e injeção de dependências

--- React-Function/src/components/FormularioCadastro/DadosUsuario.jsx
+++ React-Function/src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,20 +1,25 @@
 import { Button, TextField } from "@material-ui/core";
 import React from "react";
 
-function DadosUsuario(){
+function DadosUsuario({aoEnviar}){
     return (
-        <form>
+        <form onSubmit={(event) => {
+            event.preventDefault()
+            aoEnviar()
+        }}>
             <TextField 
                 id='email' 
                 label='email'
                 type='email'
+                required
                 variant='outlined'
                 margin='normal'
                 fullWidth></TextField>
             <TextField 
                 id='senha' 
                 label='senha' 
-                type='password' 
+                type='password'
+                required 
                 variant='outlined'
                 margin='normal'
                 fullWidth></TextField>
--- React-Function/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ React-Function/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,16 +1,23 @@
 import React, {useState} from 'react';
-import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core"
 import DadosPessoais from './DadosPessoais';
 import DadosUsuario from './DadosUsuario';
 import DadosEntrega from './DadosEntrega';
 
 function FormularioCadastro({aoEnviar, validarCPF}) {
+    const[etapaAtual, setEtapaAtual] = useState(0)
+
+    const formularios = [
+        <DadosUsuario aoEnviar={proximo}/>,
+        <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF}/>,
+        <DadosEntrega aoEnviar={aoEnviar}/>
+    ]
+    function proximo(){
+        setEtapaAtual(etapaAtual+1)
+    }
 
     return (
         <>
-            <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
-            <DadosUsuario/>
-            <DadosEntrega/>
+            {formularios[etapaAtual]}
         </>
     )
 }
