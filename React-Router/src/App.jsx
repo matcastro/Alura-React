e0ca58eb899a3ac4ed5d462b9ade1959704a5de5
Mateus Castro <matcastro2004@yahoo.com.br>
2022-02-22 20:58:01 -0300

    Início do projeto React Router já com utilização para 2 rotas.

---
+++ React-Router/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './assets/css/base/base.css'
+import Home from './paginas/Home'
+import Sobre from './paginas/Sobre'
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+
+function App() {
+  
+  return (
+  <Router>
+    <Switch>
+      <Route exact path='/'>
+        <Home/>
+      </Route>
+      <Route path='/sobre'>
+        <Sobre/>
+      </Route>
+    </Switch>
+  </Router>
+  )
+}
+
+export default App
