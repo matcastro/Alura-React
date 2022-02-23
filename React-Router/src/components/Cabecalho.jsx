7711e57ffc355407f8fcdb6b45b19e3ae0bd3ba3
Mateus Castro <matcastro2004@yahoo.com.br>
2022-02-22 21:49:54 -0300

    Link para navegação de SPA e useEffect para uso em requisições

--- React-Router/src/components/Cabecalho.jsx
+++ React-Router/src/components/Cabecalho.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import imagem from '../assets/img/doguito.svg'
 import '../assets/css/componentes/cabecalho.css'
+import { Link } from 'react-router-dom';
 
 const Cabecalho = () => {
     return(
@@ -9,18 +10,18 @@ const Cabecalho = () => {
                 <span className='menu-hamburguer__icone'></span>
             </div>
             <div className='cabecalho container'>
-                <a href='/' className='flex flex--centro'>
+                <Link to='/' className='flex flex--centro'>
                     <img className='cabecalho__logo' src={imagem} alt='Logo Doguito'></img>
                     <h1 className='cabecalho__titulo'>PetShop</h1>
-                </a>
+                </Link>
             </div>
 
             <nav className='menu-cabecalho'>
                 <ul className='menu-itens'>
-                    <li><a href='#' className='menu-item menu-item--entrar'>Entrar</a></li>
-                    <li><a href='#' className='menu-item'>Produtos</a></li>
-                    <li><a href='/' className='menu-item'>Blog</a></li>
-                    <li><a href='/sobre' className='menu-item'>Sobre</a></li>
+                    <li><Link to='#' className='menu-item menu-item--entrar'>Entrar</Link></li>
+                    <li><Link to='#' className='menu-item'>Produtos</Link></li>
+                    <li><Link to='/' className='menu-item'>Blog</Link></li>
+                    <li><Link to='/sobre' className='menu-item'>Sobre</Link></li>
                 </ul>
             </nav>
             <div className='menu-cabecalho-background'></div>
