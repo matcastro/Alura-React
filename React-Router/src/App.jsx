3c12c5692dcf29167835510559302090c72ff882
Mateus Castro <matcastro2004@yahoo.com.br>
2022-02-22 21:15:29 -0300

    Criando cabeçalho e página 404

--- React-Router/src/App.jsx
+++ React-Router/src/App.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import './assets/css/base/base.css'
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+
 import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import Pagina404 from './paginas/Pagina404'
+import Cabecalho from './components/Cabecalho'
 
 function App() {
   
   return (
   <Router>
+    <Cabecalho />
     <Switch>
       <Route exact path='/'>
         <Home/>
       </Route>
       <Route path='/sobre'>
         <Sobre/>
+      </Route>
+      <Route>
+        <Pagina404/>
       </Route>
     </Switch>
   </Router>
