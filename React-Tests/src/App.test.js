cd24ea85ecd48c8437e980bfdf50574d29b3d929
mateus.castro <castro@oceanainvestimentos.com.br>
2022-02-24 10:01:32 -0300

    Implementando o teste de uma função

--- React-Tests/src/App.test.js
+++ React-Tests/src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react'
-import App from './App'
+import App, {calcularNovoSaldo} from './App'
 
 describe('Componente principal', () => {
     describe('Quando eu abro o app do banco', () => {
@@ -16,6 +16,26 @@ describe('Componente principal', () => {
             render(<App/>)
     
             expect(screen.getByText('Realizar operação')).toBeInTheDocument()
+        })
+    })
+    describe('Quando eu realizo um transação', () => {
+        it('que é um saque, o valor vai diminuir', () => {
+            const valores = {
+                transacao: 'saque',
+                valor: 50
+            }
+            const novoSaldo = calcularNovoSaldo(valores, 150)
+
+            expect(novoSaldo).toBe(100)
+        })
+        it('que é um deposito, o valor vai aumentar', () => {
+            const valores = {
+                transacao: 'deposito',
+                valor: 50
+            }
+            const novoSaldo = calcularNovoSaldo(valores, 150)
+
+            expect(novoSaldo).toBe(200)
         })
     })
 })
